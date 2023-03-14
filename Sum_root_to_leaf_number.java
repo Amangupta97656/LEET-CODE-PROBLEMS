c5608b6ee2d768ce3d6f55af723c62e594ed3519
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-03-14 18:26:14 +0530

    Add files via upload

---
+++ Sum_root_to_leaf_number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        sum = sum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+        return dfs(root.left, sum) + dfs(root.right, sum);
+    }
+}
