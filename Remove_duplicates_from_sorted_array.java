2d0396fb9a7b62642e8df1dded547f824d78afd3
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-03-20 18:18:32 +0530

    Add files via upload

--- Remove_duplicates_from_sorted_array.java
+++ Remove_duplicates_from_sorted_array.java
@@ -1,13 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int i = 0;
-        for(int a : nums){
-            if(i ==0 || i ==1|| nums[i-2] != a){
-                nums[i] = a;
-                i++;
-            }
-        
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp1=head;
+        ListNode temp2=head;
+        if(head==null){
+            return head;
         }
-        return i;
+        while(temp2.next!=null){
+
+            temp2=temp2.next;
+            if(temp1.val!=temp2.val){
+                temp1.next=temp2;
+                temp1=temp1.next;
+            }
+        }
+        temp1.next=null;
+        return head;
+       
     }
 }
