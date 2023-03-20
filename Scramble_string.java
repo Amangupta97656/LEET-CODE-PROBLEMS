a7d450a53acc6e4489ca6e5a48e4785ab78bc8d9
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-03-20 18:18:55 +0530

    Add files via upload

---
+++ Scramble_string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isScramble(String s1, String s2) {
+        if(s1.length() != s2.length()){
+          return false;
+         }
+        memo = new HashMap<String,Boolean>();
+        boolean ans = Dp(s1,s2);
+        return ans;
+    }
+    HashMap<String,Boolean> memo;
+
+    public boolean Dp(String s1,String s2){
+        if(s1.equals(s2)){return true;}
+        if(s1.length()<=1 || s2.length()<=1){return false;}
+        String key = s1+" "+s2;
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
+        boolean flag = false;
+        int n = s1.length();
+        for(int i = 1;i<n;i++){
+            boolean condition1 = Dp(s1.substring(0,i),s2.substring(n-i,n)) && Dp(s1.substring(i,n),s2.substring(0,n-i));
+            if(condition1){flag = true;
+                break;
+            }
+            boolean condition2 = Dp(s1.substring(0,i),s2.substring(0, i)) && Dp(s1.substring(i, n),s2.substring(i, n));
+            if(condition2){flag = true;
+                break;
+            }
+        }
+        memo.put(key,flag);
+        return flag;
+    }
+}
