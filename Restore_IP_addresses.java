e97bb362ce59dff4117f826fbb0d2b329d38b71f
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-03-28 18:17:31 +0530

    Add files via upload

---
+++ Restore_IP_addresses.java
@@ -0,0 +1,28 @@
+class Solution {
+   public List<String> restoreIpAddresses(String s) {
+    List<String> ret = new LinkedList<>();
+    int[] path = new int[4];
+    helper(ret, s, 0,  path, 0);
+    return ret;
+}
+
+void helper(List<String> acc, String s, int idx, int[] path,  int segment){
+    if(segment == 4 && idx == s.length() ){
+        acc.add(path[0] + "." + path[1] + "."+ path[2] + "." + path[3]);
+        return ;
+    }else if(segment == 4 || idx == s.length() ){
+        return ;
+    }
+    
+    for(int len = 1; len <= 3 && idx + len <= s.length() ; len ++){
+        int val = Integer.parseInt(s.substring(idx, idx + len));
+        // range check, no leading 0.
+        if(val > 255 || len >= 2  && s.charAt(idx) == '0') 
+            break; 
+            
+        path[segment] = val;
+        helper(acc, s, idx + len, path, segment + 1);
+        path[segment] = -1; // for debug. 
+    }
+}
+}
