e2a253fafacc06e3dd1236f5b1c85f080c5baa1c
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-04-08 19:01:09 +0530

    Add files via upload

---
+++ Clone_graph.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public void dfs(Node node , Node copy , Node[] visited){
+        visited[copy.val] = copy;for(Node n : node.neighbors){
+            if(visited[n.val] == null){
+        Node newNode = new Node(n.val);
+    copy.neighbors.add(newNode);
+          dfs(n , newNode , visited);
+            }else{
+          copy.neighbors.add(visited[n.val]);
+            }
+        }
+        
+    }
+    public Node cloneGraph(Node node) {
+        if(node == null) return null; 
+        Node copy = new Node(node.val);
+        Node[] visited = new Node[101];
+        Arrays.fill(visited , null); 
+        dfs(node , copy , visited); 
+        return copy; 
+    }
+}
