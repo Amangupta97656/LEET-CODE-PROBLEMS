08a8da30fe080f817b839082a0ae3c9efbbe11e7
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-04-18 22:27:04 +0530

    Add files via upload

---
+++ Merge_string_alternately.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int w1Length = word1.length();
+        int w2Length = word2.length();
+        StringBuilder result = new StringBuilder();
+        int index=0;
+        
+        while(index<w1Length && index<w2Length){
+            result.append(word1.charAt(index));
+            result.append(word2.charAt(index));
+            index++;
+        }
+
+        result.append(word1.substring(index, w1Length));
+        result.append(word2.substring(index, w2Length));
+        return result.toString();
+    }
+}
