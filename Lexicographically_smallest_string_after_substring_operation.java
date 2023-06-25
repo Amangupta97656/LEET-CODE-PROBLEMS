7d3a0f9a35c32134c21cab1404588153ef449fa1
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-06-25 08:59:36 +0530

    Add files via upload

---
+++ Lexicographically_smallest_string_after_substring_operation.java
@@ -0,0 +1,13 @@
+class Solution {
+      public String smallestString(String s) {
+        int i = 0, n = s.length();
+        char[] A = s.toCharArray();
+        while (i < n && A[i] == 'a')
+            i++;
+        if (i == n)
+            A[n - 1] = 'z';
+        while (i < n && A[i] != 'a')
+            --A[i++];
+        return String.valueOf(A);
+    }
+}
