5c816dd83adc219104a8c1423bf03812fe89ab9d
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-07-25 21:36:08 +0530

    Add files via upload

---
+++ Fibonaci_number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+       if(n==0 || n==1){
+           return n;
+       }
+
+       int fib1 = fib(n-1);
+       int fib2 = fib(n-2);  
+
+       int ans = fib1 + fib2;
+
+       return ans;
+    }
+}
