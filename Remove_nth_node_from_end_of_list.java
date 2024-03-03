e9a73b9c149c99fd0cc30efc6f89550d90d12e4c
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-03-03 22:32:23 +0530

    Add files via upload

---
+++ Remove_nth_node_from_end_of_list.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int length = findLength(head);
+        int i = 0, traverseTill = length - n - 1;
+        if(traverseTill == -1) return head.next;
+        ListNode curr = head;
+        while(i < traverseTill){
+            curr = curr.next;
+            i++;
+        }
+        curr.next = curr.next.next;
+        return head;
+    }
+    public int findLength(ListNode head){
+        int count = 0;
+        if(head == null) return count;
+        ListNode curr = head;
+        while(curr != null){
+            count++;
+            curr = curr.next;
+        }
+        return count;
+    }
+}
