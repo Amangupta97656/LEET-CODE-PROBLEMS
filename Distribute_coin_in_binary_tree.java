17f334c94f851d4e0a88dfdf9a5b8d9a8d362128
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-05-18 12:41:54 +0530

    Add files via upload

---
+++ Distribute_coin_in_binary_tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int ans;
+    public int distributeCoins(TreeNode root) {
+        ans = 0;
+        countSteps(root);
+        return ans;
+    }
+    public int countSteps(TreeNode root) {
+        if (root == null) return 0;
+        int leftCoins = countSteps(root.left);
+        int rightCoins = countSteps(root.right);
+        ans += Math.abs(leftCoins) + Math.abs(rightCoins);
+        return (root.val - 1) + leftCoins + rightCoins;
+    }
+}
