f47bd80094e6fe1c21580a6b98d8be7c2aefd3e5
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-06-25 16:55:30 +0530

    Add files via upload

---
+++ Binary_search_tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    private int sum = 0;
+    
+    public TreeNode bstToGst(TreeNode root) {  
+        if (root != null) {
+            bstToGst(root.right);  // Traverse the right subtree
+            sum += root.val;  // Update the sum
+            root.val = sum;  // Update the current node's value
+            bstToGst(root.left);  // Traverse the left subtree
+        }
+        return root;
+    }
+}
