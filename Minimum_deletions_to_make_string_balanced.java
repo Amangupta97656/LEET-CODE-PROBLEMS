ffbe85657a16e4f5461f5c55e1e56f95e85ed4b8
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-07-30 08:54:42 +0530

    Add files via upload

---
+++ Minimum_deletions_to_make_string_balanced.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        char[] letters = s.toCharArray();
+        int aCount = 0;
+        int bCount = 0;
+        int output = Integer.MAX_VALUE;
+        for (int i = 0; i < letters.length; i++)
+            aCount += ('b' - letters[i]);
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            output = Math.min(output, aCount + bCount);
+            aCount -= ('b' - letters[i]);
+            bCount += (letters[i] - 'a');
+        }
+
+        output = Math.min(output, aCount + bCount);
+        return output;
+    }
+}
