1a3525d520072b3fc99e222e7630bd406a6cfe68
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-08-08 21:03:43 +0530

    Add files via upload

--- Spiral_matrix_2.java
+++ Spiral_matrix_2.java
@@ -1,72 +1,47 @@
+import java.util.*;
+
 class Solution {
-    public int[][] generateMatrix(int n) 
-    {
-        int[][] arr=new int[n][n];
-		boolean a=true;
-		boolean b=false;
-		int i=0;
-		int j=0;
-		int q=0;
-		int p=-1;
-		int r=n;
-		int s=n;
-		int count=0;
-		while(i<n&&j<n&&count<n*n)
-		{
-			count++;
-			
-			if(a==true&&b==false)
-			{
-				arr[i][j]=count;
-				j++;
-				if(j==r)
-				{
-					j--;
-					i++;
-					a=false;
-					r--;
-				}
-			}
-			else if(a==false&&b==false)
-			{
-				arr[i][j]=count;
-				i++;
-				if(i==s)
-				{
-					j--;
-					i--;
-					b=true;
-					s--;
-				}
-			}
-			else if(b==true&&a==false)
-			{
-				arr[i][j]=count;
-				//System.out.print("(i:"+i+" j:"+j+") ");
-				j--;
-				if(j==p)
-				{
-					i--;
-					j++;
-					a=true;
-					p++;
-					
-				}
-			}
-			else if(b==true&&a==true)
-			{
-				arr[i][j]=count;
-				i--;
-				if(i==q)
-				{
-					i++;
-					j++;
-					b=false;
-					q++;
-				}
-				
-			}
-		}
-        return arr;
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int numSteps = 1;
+        int totalCells = rows * cols;
+        List<int[]> result = new ArrayList<>();
+        int r = rStart, c = cStart;
+        int d = 0;
+
+        while (result.size() < totalCells) {
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < numSteps; j++) {
+                    if (0 <= r && r < rows && 0 <= c && c < cols) {
+                        result.add(new int[]{r, c});
+                    }
+                    if (result.size() == totalCells) {
+                        return convertListToArray(result);
+                    }
+                    r += directions[d][0];
+                    c += directions[d][1];
+                }
+                d = (d + 1) % 4;
+            }
+            numSteps++;
+        }
+
+        return convertListToArray(result);
+    }
+
+    private int[][] convertListToArray(List<int[]> list) {
+        int[][] array = new int[list.size()][2];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] result = solution.spiralMatrixIII(5, 6, 1, 4);
+        for (int[] coords : result) {
+            System.out.println(Arrays.toString(coords));
+        }
     }
 }
