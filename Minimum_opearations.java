227938aa6899e9907b53160b29ceab30e4ef6964
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-03-19 21:08:58 +0530

    Add files via upload

---
+++ Minimum_opearations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int m = 0;
+        for (int i = 0; i <= n - 3; i++) 
+        {
+            if (nums[i] == 0) 
+            {
+                nums[i] ^= 1;
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+                m++;
+            }
+        }
+        for (int num : nums) 
+        {
+            if (num == 0)
+            {
+                return -1;
+            }    
+        }
+        return m;
+    }
+}
