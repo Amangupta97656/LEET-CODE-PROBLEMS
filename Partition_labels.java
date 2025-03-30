d96200c7ce240e9bf4d9a681964dfbaaf5c333a6
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-03-30 21:35:22 +0530

    Add files via upload

---
+++ Partition_labels.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+
+        int[] lastOccurrence = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            lastOccurrence[s.charAt(i) - 'a'] = i;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastOccurrence[s.charAt(i) - 'a']);
+            if (i == end) {
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+    }
+        
+}
