e79a1c55ce7f69c55b42bd11d5850d1eb1100bbc
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-04-13 22:58:05 +0530

    Add files via upload

---
+++ Count_good_numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+    long mod=1000000007;
+    public int countGoodNumbers(long n) {
+      long even=  (n+1)/2;
+      long odd=n/2;
+      return (int)(pow(5,even)*pow(4,odd)%mod);
+    }
+    public long pow(long x,long n)
+    {
+        if(n==0)
+        return 1;
+        long temp=pow(x,n/2);
+        if(n%2==0)
+        {
+            return (temp*temp)%mod;
+
+        }
+        else
+        {
+            return (x*temp*temp)%mod;
+        }
+    }
+}
